4808039e629b9a09e1cf671ecaefac7e1150d22d
kaelanlupton <16kl15@queensu.ca>
2019-03-28 14:03:53 -0400

    added forms for each section

---
+++ peopleForm.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="firstPage.css" type="text/css" rel="stylesheet" >
+</head>
+<body>
+<h2>People Information</h2>
+
+
+<?php ?>
+</table>
+</body>
+</html> 
